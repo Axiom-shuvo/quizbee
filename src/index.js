8d8cc776ebf35b730581056268569e111bb88044
unknown <techshuvo@gmail.com>
2020-05-26 20:52:11 +0600

    set state , update & map the sate to display date

--- src/index.js
+++ src/index.js
@@ -1,2 +1,43 @@
 import React,{Component} from "react";
-import "./assets/styles.css";
+import ReactDOM from "react-dom";
+import "./assets/style.css";
+import quizService from "./quizService";
+
+
+class QuizBee extends Component{
+
+    state={
+        questionBank: []
+    };
+
+    getQuestions= () => {
+        quizService().then(
+            question => {
+                this.setState({
+                    questionBank: question
+                })
+            }
+        )
+    }
+
+    componentDidMount(){
+        this.getQuestions();
+    }
+    
+
+    render(){
+        return(
+            <div className="container" >
+                <div className="title" >QuizBee</div>
+                { this.state.questionBank.length > 0 &&
+                  this.state.questionBank.map(
+                      ( { question,ansers,correct,questionId } ) => <h4>{question}</h4> 
+                    )}
+            </div>
+        )
+    }
+}
+
+
+ReactDOM.render(<QuizBee />,document.getElementById("root"));
+
